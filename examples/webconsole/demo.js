0163f5283142d542ab6f4c812a23317218f87548
Dave Pacheco <dap@joyent.com>
2012-04-15 12:17:45 -0700

    prototype web console

---
+++ examples/webconsole/demo.js
@@ -0,0 +1,97 @@
+/*
+ * demo.js: static-file node HTTP server for demos
+ *
+ * Usage: node demo.js [port]
+ *
+ *    Sets up a web server on the given port (or port 80) serving static files
+ *    out of the given path.
+ */
+
+var mod_fs = require('fs');
+var mod_http = require('http');
+var mod_path = require('path');
+var mod_url = require('url');
+
+var mod_kang = require('../../lib/kang');
+
+var dd_index = 'index.htm';
+var dd_cwd = __dirname;
+var dd_port = 80;
+
+var i;
+
+for (i = 2; i < process.argv.length; i++) {
+	dd_port = parseInt(process.argv[i], 10);
+	if (isNaN(dd_port)) {
+		console.error('usage: node demo.js [port]');
+		process.exit(1);
+	}
+}
+
+mod_http.createServer(function (req, res) {
+	var uri = mod_url.parse(req.url).pathname;
+	var path;
+	var filename;
+
+	path = (uri == '/') ? dd_index : uri;
+
+	if (path == '/proxy')
+		return (kangProxy(req, res));
+
+	filename = mod_path.join(dd_cwd, path);
+
+	mod_fs.readFile(filename, function (err, file) {
+		if (err) {
+			res.writeHead(404);
+			res.end();
+			return;
+		}
+
+		res.writeHead(200);
+		res.end(file);
+	});
+}).listen(dd_port, function () {
+	console.log('HTTP server started on port ' + dd_port);
+});
+
+function kangProxy(request, response)
+{
+	var hosts = mod_url.parse(request.url, true).query.host;
+	var allsources, validsources;
+
+	if (!hosts) {
+		response.writeHead(200);
+		return (response.end(JSON.stringify({
+			sources: [], snapshot: {}
+		})));
+	}
+
+	if (!Array.isArray(hosts))
+		hosts = [ hosts ];
+
+	allsources = [];
+	validsources = [];
+
+	hosts.forEach(function (host) {
+		try {
+			var source = mod_kang.knMakeSource(host);
+			validsources.push(source);
+			allsources.push({ host: host, source: source });
+		} catch (ex) {
+			allsources.push({ host: host, error: ex.message });
+		}
+	});
+
+	mod_kang.knFetchAll({ sources: validsources }, function (err, snapshot) {
+		var rv = {
+			sources: allsources,
+			snapshot: snapshot
+		};
+
+		if (err)
+			rv['error'] = err;
+
+		response.writeHead(200);
+		response.end(JSON.stringify(rv));
+	});
+}
---
+++ examples/webconsole/resources/js/console.js
@@ -0,0 +1,247 @@
+/*
+ * console.js: kang web console implementation
+ */
+
+$(document).ready(kInit);
+
+/*
+ * Important DOM elements
+ */
+var kDataContainer;
+var kServiceField;
+var kServiceSummary;
+var kServiceTable;
+var kDump;
+
+/*
+ * Application state
+ */
+var kServiceUrls = [];  /* list of URLs we're collecting data from */
+var kServices = [];	/* list of resolved services */
+var kSnapshot;
+
+function kInit()
+{
+	kDataContainer = document.getElementById('kDataContainer');
+	kServiceField = document.getElementById('kAddServiceService');
+	kServiceTable = document.getElementById('kServiceTable');
+	kServiceSummary = document.getElementById('kServiceSummary');
+	kDump = document.getElementById('kDump');
+
+	kMakeVisibleToggle(document.getElementById('kDumpToggle'),
+	    kDump, false);
+
+	kRedrawWorld();
+}
+
+function kFormAddService()
+{
+	if (kServiceField.value.length > 0)
+		kServiceUrls.push(kServiceField.value);
+	kServiceField.value = '';
+	kRefresh();
+}
+
+function kFormRefreshAll()
+{
+	kRefresh();
+}
+
+function kRedrawWorld()
+{
+	kRedrawServices();
+	kRedrawDynamic();
+	kRedrawDump();
+}
+
+function kRedrawServices()
+{
+	var table = $(kServiceTable).dataTable({
+		'aaData': kServices,
+		'aoColumns': [
+			{ 'sTitle': 'URL', 'sWidth': '30%' },
+			{ 'sTitle': 'Service', 'sWidth': '30%' },
+			{ 'sTitle': 'Component', 'sWidth': '10%' },
+			{ 'sTitle': 'Instance', 'sWidth': '30%' }
+		],
+		'bAutoWidth': false,
+		'bDestroy': true,
+		'bFilter': false,
+		'bInfo': false,
+		'bPaginate': false,
+		'bLengthChange': false,
+		'oLanguage': {
+			'sEmptyTable': 'No services added yet.'
+		}
+	});
+
+	$(kServiceSummary).height($(kServiceTable).height() + 70);
+}
+
+function kRedrawDynamic()
+{
+	while (kDataContainer.firstChild !== null)
+		kDataContainer.removeChild(kDataContainer.firstChild);
+
+	if (!kSnapshot)
+		return;
+
+	/* XXX should use library client code */
+	var snapshot = kSnapshot['snapshot'].cs_objects;
+	var name, div, objkeys, fields, field, rows, columns;
+
+	for (var key in snapshot) {
+		if (key == 'service' || key == 'stats')
+			continue;
+
+		name = key[0].toUpperCase() + key.substr(1);
+
+		div = kDataContainer.appendChild(
+		    kMakeElement('div', 'kSubHeader'));
+		div.appendChild(document.createTextNode(name));
+
+		objkeys = Object.keys(snapshot[key]);
+		if (objkeys.length === 0) {
+			columns = [ { 'sTitle': key } ];
+			rows = [];
+		} else {
+			columns = [];
+			rows = [];
+			fields = [];
+			for (field in snapshot[key][objkeys[0]][0]) {
+				fields.push(field);
+				columns.push({ 'sTitle': field });
+			}
+
+			objkeys.forEach(function (objkey) {
+				snapshot[key][objkey].forEach(function (entry) {
+					var row = [];
+					fields.forEach(function (field) {
+						row.push(entry[field] || '');
+					});
+					rows.push(row);
+				});
+			});
+		}
+
+		div = kDataContainer.appendChild(
+		    kMakeElement('table', 'kDynamicTable'));
+		$(div).dataTable({
+			'aaData': rows,
+			'aoColumns': columns,
+			'iDisplayLength': 40,
+			'bLengthChange': false,
+			'bFilter': false,
+			'bInfo': false,
+			'oLanguage': {
+				'sEmptyTable': 'No objects.'
+			}
+		});
+
+		kDataContainer.appendChild(
+		    kMakeElement('div', 'kHorizontalSeparator'));
+	}
+}
+
+function kRedrawDump()
+{
+	var text = document.createTextNode(JSON.stringify(kSnapshot, null, 4));
+
+	if (kDump.firstChild)
+		kDump.replaceChild(text, kDump.firstChild);
+	else
+		kDump.appendChild(text);
+}
+
+function kMakeElement(tag, classes)
+{
+	var rv = document.createElement(tag);
+	if (classes)
+		rv.className = classes;
+	return (rv);
+}
+
+function kMakeHeader(text)
+{
+	var rv = kMakeElement('h2');
+	rv.appendChild(document.createTextNode(text));
+	return (rv);
+}
+
+function kMakeVisibleToggle(toggle, element, visible)
+{
+	var set_state = function (nowvisible) {
+		if (nowvisible) {
+			toggle.innerHTML = 'hide';
+			element.style.display = 'block';
+		} else {
+			toggle.innerHTML = 'show';
+			element.style.display = 'none';
+		}
+
+		state = nowvisible;
+	};
+
+	set_state(visible);
+
+	$(toggle).click(function () { set_state(!state); });
+}
+
+function kRefresh()
+{
+	var query = '?' + kServiceUrls.map(function (url) {
+		return ('host=' + encodeURIComponent(url));
+	}).join('&');
+
+	var rq = $.getJSON('proxy' + query, function (data) {
+		if (!data || !data['snapshot'] || !data['snapshot'].cs_objects)
+			return;
+
+		/*
+		 * Go through and normalize our source names.
+		 */
+		var i, source, name;
+		for (i = 0; i < data['sources'].length; i++) {
+			if (data['sources'][i].hasOwnProperty('error'))
+				continue;
+
+			source = data['sources'][i]['source'];
+			name = source['protocol'] + '://' +
+			    source['host'] + ':' + source['port'] +
+			    source['path'];
+			kServiceUrls[i] = name;
+		}
+
+		var snapshot = data['snapshot'];
+		var services = snapshot.cs_objects['service'];
+		kServices = [];
+		if (!services)
+			return;
+		Object.keys(services).forEach(
+		    function (key) {
+			/* XXX auto-generate data table? */
+			var service = services[key][0]; /* XXX [0] */
+			kServices.push([
+				service['source'],
+				service['name'],
+				service['component'] || '',
+				service['ident']
+			]);
+		});
+
+		kSnapshot = data;
+		kRedrawWorld();
+	});
+
+	rq.error(function (_, errmsg, err) {
+		var msgs = [ 'refresh failed' ];
+
+		if (errmsg)
+			msgs.push(errmsg);
+
+		if (err)
+			msgs.push(err.message);
+
+		alert(msgs.join(': '));
+	});
+}
