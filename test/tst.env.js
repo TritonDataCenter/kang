307c3e5275b6a6df6e16efdb12bdd3ef7617be12
Cody Peter Mello <cody.mello@joyent.com>
2017-02-06 05:29:46 +0000

    davepacheco/kang#3 Support IPv6 addresses in URLs and check ports more strictly

--- test/tst.env.js
+++ test/tst.env.js
@@ -68,6 +68,90 @@ mod_assert.deepEqual(mod_kang.knMakeSource('example/bar'), {
 	path: '/bar'
 });
 
+mod_assert.deepEqual(mod_kang.knMakeSource('127.0.0.1'), {
+	protocol: 'http',
+	host: '127.0.0.1',
+	port: 80,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('127.0.0.1/foo'), {
+	protocol: 'http',
+	host: '127.0.0.1',
+	port: 80,
+	path: '/foo'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('127.0.0.1:8080'), {
+	protocol: 'http',
+	host: '127.0.0.1',
+	port: 8080,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('127.0.0.1:8080/foo'), {
+	protocol: 'http',
+	host: '127.0.0.1',
+	port: 8080,
+	path: '/foo'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('[::1]'), {
+	protocol: 'http',
+	host: '::1',
+	port: 80,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('https://[::1]'), {
+	protocol: 'https',
+	host: '::1',
+	port: 443,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('[::1]:8080'), {
+	protocol: 'http',
+	host: '::1',
+	port: 8080,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('[fe80::92b8:d0ff:fe4b:c73b]'), {
+	protocol: 'http',
+	host: 'fe80::92b8:d0ff:fe4b:c73b',
+	port: 80,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('[fd00::1]'), {
+	protocol: 'http',
+	host: 'fd00::1',
+	port: 80,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('[fd00::1]/bar'), {
+	protocol: 'http',
+	host: 'fd00::1',
+	port: 80,
+	path: '/bar'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('[fd00::1]:8080'), {
+	protocol: 'http',
+	host: 'fd00::1',
+	port: 8080,
+	path: '/kang/snapshot'
+});
+
+mod_assert.deepEqual(mod_kang.knMakeSource('[fd00::1]:8080/baz'), {
+	protocol: 'http',
+	host: 'fd00::1',
+	port: 8080,
+	path: '/baz'
+});
+
 mod_assert.throws(function () { mod_kang.knMakeSource(':8080'); },
     /no host specified/);
 
@@ -83,8 +167,44 @@ mod_assert.throws(function () { mod_kang.knMakeSource('http://'); },
 mod_assert.throws(function () { mod_kang.knMakeSource('http:///bar'); },
     /no host specified/);
 
+mod_assert.throws(function () { mod_kang.knMakeSource('example:65536'); },
+    /invalid port: 65536 \(must be in range 1-65535\)$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('example:0'); },
+    /invalid port: 0 \(must be in range 1-65535\)$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('example:-1'); },
+    /invalid port: invalid number: "-1"$/);
+
 mod_assert.throws(function () { mod_kang.knMakeSource('example:foo'); },
-    /invalid port/);
+    /invalid port: invalid number: "foo"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('example:123b'); },
+    /invalid port: trailing characters after number: "b"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('example:0x1'); },
+    /invalid port: trailing characters after number: "x1"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('example:0xa'); },
+    /invalid port: trailing characters after number: "xa"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('example:1.2'); },
+    /invalid port: trailing characters after number: ".2"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('example:1\t2'); },
+    /invalid port: trailing characters after number: "\\t2"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('1.2.3.400'); },
+    /bad IPv4 address: "1.2.3.400"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('256.2.3.4'); },
+    /bad IPv4 address: "256.2.3.4"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('[::12345]'); },
+    /bad IPv6 address: "::12345"$/);
+
+mod_assert.throws(function () { mod_kang.knMakeSource('[:1:]'); },
+    /bad IPv6 address: ":1:"$/);
 
 mod_assert.deepEqual(mod_kang.knParseSources(''), []);
 mod_assert.deepEqual(mod_kang.knParseSources('moe'), [ {
@@ -93,6 +213,7 @@ mod_assert.deepEqual(mod_kang.knParseSources('moe'), [ {
 	port: 80,
 	path: '/kang/snapshot'
 }]);
+
 mod_assert.deepEqual(mod_kang.knParseSources('moe,apu,ralph:8080/catfood'), [ {
 	protocol: 'http',
 	host: 'moe',
@@ -106,6 +227,25 @@ mod_assert.deepEqual(mod_kang.knParseSources('moe,apu,ralph:8080/catfood'), [ {
 }, {
 	protocol: 'http',
 	host: 'ralph',
+	port: 8080,
+	path: '/catfood'
+} ]);
+
+mod_assert.deepEqual(
+    mod_kang.knParseSources('127.0.0.1/foo,[::1]:8090,localhost:8080/catfood'),
+    [ {
+	protocol: 'http',
+	host: '127.0.0.1',
+	port: 80,
+	path: '/foo'
+}, {
+	protocol: 'http',
+	host: '::1',
+	port: 8090,
+	path: '/kang/snapshot'
+}, {
+	protocol: 'http',
+	host: 'localhost',
 	port: 8080,
 	path: '/catfood'
 } ]);
